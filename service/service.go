f4f64e3b174c3802c6e37bcd11df5b6f7ee68d76
tinylcy <tinylcy@gmail.com>
2018-02-02 20:00:02 +0800

    Add HTTP service module implementation.

---
+++ service/service.go
@@ -0,0 +1,183 @@
+package service
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"strings"
+)
+
+// Store is the interface Raft-backed key-value stores must implement.
+type Store interface {
+	// Return the value for the given key.
+	Get(key string) (string, error)
+
+	// Set the value for the given key, via distributed consensus.
+	Set(key string, value string) error
+
+	// Delete the given key and its value, via distributed consensus.
+	Delete(key string) error
+
+	// Join the node identidied by nodeId and address.
+	Join(nodeId string, addr string) error
+}
+
+// Service provoides HTTP service.
+type Service struct {
+	addr string
+	ln   net.Listener
+
+	store Store
+}
+
+// Return an uninitialized HTTP service.
+func New(addr string, store Store) *Service {
+	return &Service{
+		addr:  addr,
+		store: store,
+	}
+}
+
+// Start the service.
+func (s *Service) Start() error {
+	server := http.Server{
+		Handler: s,
+	}
+
+	ln, err := net.Listen("tcp", s.addr)
+	if err != nil {
+		return err
+	}
+	s.ln = ln
+
+	http.Handle("/", s)
+
+	go func() {
+		err := server.Serve(s.ln)
+		if err != nil {
+			log.Fatalf("HTTP serve: %s", err)
+		}
+	}()
+
+	return nil
+}
+
+// Close the service.
+func (s *Service) Close() {
+	s.ln.Close()
+	return
+}
+
+// ServeHTTP allows service to serive HTTP requests.
+func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if strings.HasPrefix(r.URL.Path, "/key") {
+		s.handleKeyRequest(w, r)
+	} else if strings.HasPrefix(r.URL.Path, "/join") {
+		s.handleJoin(w, r)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
+func (s *Service) handleKeyRequest(w http.ResponseWriter, r *http.Request) {
+	getKey := func() string {
+		parts := strings.Split(r.URL.Path, "/")
+		if len(parts) != 3 {
+			return ""
+		}
+		return parts[2]
+	}
+
+	switch r.Method {
+
+	case "GET":
+		k := getKey()
+		if k == "" {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		v, err := s.store.Get(k)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		b, err := json.Marshal(map[string]string{k: v})
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		io.WriteString(w, string(b))
+
+	case "POST":
+		// Read the value from the POST body
+		m := map[string]string{}
+		if err := json.NewDecoder(r.Body).Decode(&m); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		for k, v := range m {
+			if err := s.store.Set(k, v); err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+		}
+
+	case "DELETE":
+		k := getKey()
+		if k == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if err := s.store.Delete(k); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		s.store.Delete(k)
+
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+
+	}
+
+	return
+}
+
+func (s *Service) handleJoin(w http.ResponseWriter, r *http.Request) {
+	m := map[string]string{}
+	if err := json.NewDecoder(r.Body).Decode(&m); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if len(m) != 2 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	remoteAddr, ok := m["addr"]
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	nodeID, ok := m["id"]
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if err := s.store.Join(nodeID, remoteAddr); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+}
+
+// Return the address on which the service is listening.
+func (s *Service) Addr() net.Addr {
+	return s.ln.Addr()
+}
